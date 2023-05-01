c0c893cecb7835bc1343e4be9c6ea095ea1f611f
Yablokova Irina <yablokovairinav@yandex.ru>
2023-05-01 17:16:25 +0300

    Атака клонов 1.0

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# yamdb_final
-yamdb_final
+# Yamdb_final
+![example workflow](https://github.com/github/docs/actions/workflows/main.yml/badge.svg)
+## Описание проекта:
+Проект YaMDb собирает отзывы пользователей на произведения. Сами произведения в YaMDb не хранятся, здесь нельзя посмотреть фильм или послушать музыку.   
+Произведения делятся на категории, а также им может быть присвоен жанр из списка предустановленных (например, «Сказка», «Рок» или «Артхаус»).  
+Благодарные или возмущённые пользователи оставляют к произведениям текстовые отзывы и ставят произведению оценку в диапазоне от одного до десяти; из пользовательских оценок формируется усреднённая оценка произведения — рейтинг.
+Пользователи могут оставлять комментарии к отзывам.
+
+
+### Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone git@github.com:YablokovaIrina/yamdb_final.git
+```
+
+```
+$ cd infra
+```
+
+Запустить
+
+```
+$ docker-compose up -d --build
+```
+
+Выполнить миграции 
+
+```
+$ docker-compose exec web python manage.py makemigrations
+$ docker-compose exec web python manage.py migrate
+```
+
+Создать суперпользователя
+
+```
+$ docker-compose exec web python manage.py createsuperuser
+```
+
+Собрать статику
+
+```
+$ docker-compose exec web python manage.py collectstatic --no-input
+```
+
+Остановить контейнеры
+
+```
+$ docker-compose down -v
+```
+
+Шаблон наполнения .env (не включен в текущий репозиторий) расположенный по пути infra/.env
+
+```
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db
+DB_PORT=5432
+```
+
+Документация: http://127.0.0.1/redoc/
+
+### Использованные технологии:
+Python 3.9  
+Django 3.2  
+DRF  
+Docker
+
+
+### Авторы проекта:
+[Яблокова Ирина](https://github.com/YablokovaIrina) 
---
+++ api_yamdb/api_yamdb/settings.py
@@ -0,0 +1,130 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY', default='key')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
+    'rest_framework',
+    'django_filters',
+    'api',
+    'reviews',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api_yamdb.urls'
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'api_yamdb.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE',
+                            default='django.db.backends.postgresql'
+                            ),
+        'NAME': os.getenv('DB_NAME', default='default'),
+        'USER': os.getenv('POSTGRES_USER', default='default'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='default'),
+        'HOST': os.getenv('DB_HOST', default='default'),
+        'PORT': os.getenv('DB_PORT', default='default')
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'reviews.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+EMAIL_LENGHT = 254
+MAX_LENGHT = 150
+MAX_LENGHT_CODE = 50
+EMAIL_YAMDB = 'registration_YaMDb@mail.com'
+FORBIDDEN_NAME = 'me'
+USER_EXISTS_MESSAGE = 'Пользователь с таким username уже зарегистрирован'
+EMAIL_EXISTS_MESSAGE = 'Указанная почта уже зарегестрирована другим пользователем'
